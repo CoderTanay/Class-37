788d5f77ae71f786706d29fc85b29d6bcff5ffae
Tanay <tanaykatyal@icloud.com>
2021-01-10 13:48:45 -0500

    Made realtime database

--- sketch.js
+++ sketch.js
@@ -1,29 +1,43 @@
 var ball;
+var db;
+var ballPos;
+var pos;
 
 function setup(){
     createCanvas(500,500);
+    db = firebase.database();
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+    ballPos = db.ref("ball/position");
+    ballPos.on("value", read);
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+    db.ref("ball/position").set({
+        x: pos.x + x,
+        y: pos.y + y
+    })
+}
+
+function read(data){
+    pos = data.val();
+    ball.x = pos.x;
+    ball.y = pos.y;
 }
