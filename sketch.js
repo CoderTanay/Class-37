4eefcbb4030673026ed71a5f2e6ee0c7363560c1
Tanay <tanaykatyal@icloud.com>
2021-01-20 13:00:11 -0500

    Started Game

--- sketch.js
+++ sketch.js
@@ -1,43 +1,22 @@
 var ball;
-var db;
+var database;
 var ballPos;
 var pos;
 
+var playerCount = 0;
+var gameState = 0;
+
+var form, game, player;
+
 function setup(){
-    createCanvas(500,500);
-    db = firebase.database();
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
-    ballPos = db.ref("ball/position");
-    ballPos.on("value", read);
+    createCanvas(400,400);
+    database = firebase.database();
+    game = new Game()
+    game.getState();
+    game.start();
 }
 
 function draw(){
-    background("white");
-    if(keyDown(LEFT_ARROW)){
-        writePosition(-1,0);
-    }
-    else if(keyDown(RIGHT_ARROW)){
-        writePosition(1,0);
-    }
-    else if(keyDown(UP_ARROW)){
-        writePosition(0,-1);
-    }
-    else if(keyDown(DOWN_ARROW)){
-        writePosition(0,+1);
-    }
-    drawSprites();
-}
-
-function writePosition(x,y){
-    db.ref("ball/position").set({
-        x: pos.x + x,
-        y: pos.y + y
-    })
-}
-
-function read(data){
-    pos = data.val();
-    ball.x = pos.x;
-    ball.y = pos.y;
+    background("lightblue");
+    
 }
