d1c90df10bf7f0ff37421fa406745dc92a912961
Tanay <tanaykatyal@icloud.com>
2021-01-31 12:56:02 -0500

    made game

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 var database;
 var ballPos;
 var pos;
-
+var car1, car2, car3, car4, cars
 var playerCount = 0;
 var gameState = 0;
 var allPlayers;
@@ -9,7 +9,7 @@ var allPlayers;
 var form, game, player;
 
 function setup(){
-    createCanvas(400,400);
+    createCanvas(displayWidth - 20,displayHeight - 30);
     database = firebase.database();
     game = new Game()
     game.getState();
